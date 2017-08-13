553bae4016d825aafca4b03a1836fd3065700370
Roman <roman@Ivanovs-MacBook-Pro.local>
2017-08-14 01:08:31 +0700

    Initial Commit

---
+++ Time Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Manager
+//
+//  Created by Roman on 8/14/17.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
